24566c53bdb82cc31fbf74a0c80eda87bbab7e8d
Wos <wosleymr@gmail.com>
2025-01-22 22:11:06 -0300

    feat: improve killzone

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,10 +3,13 @@ extends Area2D
 @onready var timer = $Timer
 
 
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+func _on_body_entered(body):
 	print("You DIED!!!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
