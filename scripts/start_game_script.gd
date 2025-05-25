40970237afbbdf5f30b76c8bb9717b97ba2e2888
Wos <wosleymr@gmail.com>
2025-05-25 12:04:49 -0300

    feat: control start game script to choose between main or develop branches scenes

--- scripts/start_game_script.gd
+++ scripts/start_game_script.gd
@@ -1,4 +1,7 @@
 extends Button
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/develop_branch.tscn")
+	if self.text.contains("feature"):
+		get_tree().change_scene_to_file("res://scenes/develop_branch.tscn")
+	elif self.text.contains("hotfix"):
+		get_tree().change_scene_to_file("res://scenes/main_branch.tscn")
