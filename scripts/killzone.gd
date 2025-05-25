28e7903e19f962d04945c65691d5997c0480e326
Wos <wosleymr@gmail.com>
2025-05-25 12:01:35 -0300

    fix: change killzone time behavior

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -6,7 +6,7 @@ signal playerDied
 
 func _on_body_entered(body):
 	print("You DIED!!!")
-	Engine.time_scale = 0.5
+	Engine.time_scale = 0.4
 	body.get_node("CollisionShape2D").set_deferred("disabled", true)
 	timer.start()
 
